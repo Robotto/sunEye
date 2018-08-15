1acecad031902399ea7dea68a300c369ec19d2e6
Mark Moore <mark@moore.dk>
2018-08-15 19:30:32 +0200

    added constant latitudal circle for my position

--- server/ovation/procedure.sh
+++ server/ovation/procedure.sh
@@ -9,6 +9,5 @@ wget https://services.swpc.noaa.gov/images/animations/ovation-north/latest.jpg -
 
 
 #Combined:
-mogrify -crop 600X600+95+100 +repage -rotate "90" -resize 240x240 -fill white -stroke white -strokewidth 1 -draw "line 151,0 151,240" -draw "line 0,206 239,206" ovation.jpg
+mogrify -crop 600X600+95+100 +repage -rotate "90" -resize 240x240 -fill white -stroke white -strokewidth 1 -draw "line 151,0 151,240" -draw "line 0,206 239,206" -draw "fill none circle 120,122 151,206" ovation.jpg
 #convert ovation.jpg -fill white -stroke white -strokewidth 1 -draw "line 151,0 151,240" -draw "line 0,206 239,206" linetest.jpg
-

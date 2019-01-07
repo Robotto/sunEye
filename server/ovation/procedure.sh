9e21289735bdb990962937f5e48c0ff41d660870
Mark Moore <mark@moore.dk>
2019-01-08 00:06:52 +0100

    added some more kp index fun

--- server/ovation/procedure.sh
+++ server/ovation/procedure.sh
@@ -7,9 +7,19 @@ wget https://services.swpc.noaa.gov/images/animations/ovation-north/latest.jpg -
 
 #get current KP index (tail gets latest line, sed outputs everything after the last space in the line, head outputs current KP)
 KP=$(curl -s https://services.swpc.noaa.gov/text/daily-geomagnetic-indices.txt | tail -n 1 | sed 's/.*\ //' | head -c 1 2>&1)
-#convert latest.jpg -crop 600X600+95+100 +repage cropped.jpg
-#mogrify -rotate "90" cropped.jpg 
 
 
-#Combined:                                                                                                       Horizontal line               Vertical line              Marker circle                            Circle center Marker
-mogrify -crop 600X600+95+100 +repage -rotate "90" -resize 240x240 -fill white -stroke white -strokewidth 1 -draw "line 151,0 151,240" -draw "line 0,206 239,206" -draw "fill none circle 97,135 151,206" -draw "fill red circle 96.5,134.5 97.5,135.5" -stroke white -strokewidth 1 -annotate +10+20 "KP: $KP" ovation.jpg
+#colorize kp index text:
+kpColour=green
+if [ $((KP)) -ge 4 ]
+then
+kpColour=yellow
+fi
+if [ $((KP)) -ge 6 ]
+then
+kpColour=red
+fi
+
+
+#Combined:                                                                                                       Horizontal line               Vertical line              Marker circle                            Circle center Marker                                     #add kp index text
+mogrify -crop 600X600+95+100 +repage -rotate "90" -resize 240x240 -fill white -stroke white -strokewidth 1 -draw "line 151,0 151,240" -draw "line 0,206 239,206" -draw "fill none circle 97,135 151,206" -draw "fill red circle 96.5,134.5 97.5,135.5" -stroke $kpColour -fill $kpColour -strokewidth 1 -pointsize 25 -annotate +10+30 "kp: $KP" ovation.jpg
